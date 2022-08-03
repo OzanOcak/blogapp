d180c8c0cbec2172d8fb4cde3ef693432402b1e7
OzanOcak <ozanocak@gmail.com>
2022-08-03 18:28:11 -0400

    mongoose setup & connecting mondo db

--- pages/api/create.js
+++ pages/api/create.js
@@ -1,6 +1,10 @@
-export default function handler(req, res) {
+import { dbConnect } from "../../lib/db-connect";
+
+export default async function handler(req, res) {
   if (req.method !== "POST")
     res.status(400).json({ massage: "invalid request!" });
+
+  await dbConnect();
 
   const { name } = req.body;
 
@@ -9,3 +13,6 @@ export default function handler(req, res) {
 
 // curl -X POST localhost:3000/api/create
 // will get {"name":"post is created"} respond
+
+// with curl post , server responds new connection is created and later it will
+//respond it is connected existing db
