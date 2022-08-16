f77855faf6090e3290a38774b8be729ae5240e1a
OzanOcak <ozanocak@gmail.com>
2022-08-16 16:10:27 -0400

    showing state in header

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,32 +1,50 @@
 import Head from "next/head";
 import Link from "next/link";
 import { useStore } from "../../client/context";
+import { getValue } from "../utils/common";
 
 const Header = () => {
   const [state, dispatch] = useStore();
+  const user = getValue(state, ["user"]);
+  const authenticated = getValue(state, ["user", "authenticated"], false); //state.user.authenticated
 
   console.log({ state });
   return (
     <header>
       <nav className=" flex gap-5 m-2 justify-between items-end text-blue-500  border-b">
-        <Link href={"/"}>
-          <a className="text-[1.2rem] mb-3 text-gray-400">O.Ocak</a>
-        </Link>
+        {authenticated ? (
+          <Link href={"/"}>
+            <a className="text-[1.2rem] mb-3 text-gray-400">{user.name}</a>
+          </Link>
+        ) : (
+          <Link href={"/"}>
+            <a className="text-[1.2rem] mb-3 text-gray-400">welcome guest</a>
+          </Link>
+        )}
+
         <Link href={"/"}>
           <a className="text-[3rem] font-bold">BlogX</a>
         </Link>
-        <div>
-          <button className="mb-3 px-2 py-2 border rounded-[1rem] mr-2 hover:bg-blue-500 hover:text-[white]">
-            <Link href={"/login"}>
-              <a>Log In</a>
-            </Link>
-          </button>
+        {authenticated ? (
           <button className="mb-3 px-2 py-2 border rounded-[1rem] hover:bg-blue-500 hover:text-[white]">
             <Link href={"/signup"}>
-              <a>Sign Up</a>
+              <a>Log Out</a>
             </Link>
           </button>
-        </div>
+        ) : (
+          <div>
+            <button className="mb-3 px-2 py-2 border rounded-[1rem] mr-2 hover:bg-blue-500 hover:text-[white]">
+              <Link href={"/login"}>
+                <a>Log In</a>
+              </Link>
+            </button>
+            <button className="mb-3 px-2 py-2 border rounded-[1rem] hover:bg-blue-500 hover:text-[white]">
+              <Link href={"/signup"}>
+                <a>Sign Up</a>
+              </Link>
+            </button>
+          </div>
+        )}
       </nav>
     </header>
   );
